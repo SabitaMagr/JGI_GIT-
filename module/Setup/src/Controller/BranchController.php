513efc6728208b4cbdbb340e92d02a1ff951dfc9
Somkala <somkala.pachhai@itnepal.com>
2016-08-02 15:51:46 +0545

    branch updated

--- module/Setup/src/Controller/BranchController.php
+++ module/Setup/src/Controller/BranchController.php
@@ -5,14 +5,31 @@ namespace Setup\Controller;
 use Zend\Form\Annotation\AnnotationBuilder;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Setup\Model\Branch;
 
 class BranchController extends AbstractActionController{
+
+	protected $form;
+
+	public function getForm(){
+		$branch = new Branch();
+		$builder = new AnnotationBuilder();
+		if (!$this->form) {
+			$this->form = $builder->createForm($branch);
+		}
+		return $this->form;
+	}
 
 	public function indexAction(){
 
 	}
 	public function addAction(){
-		return new ViewModel();
+		$form = $this->getForm();
+
+        return new ViewModel([
+            'form' => $form,
+            'messages' => $this->flashmessenger()->getMessages()
+        ]);
 	}
 	public function editAction(){
 
