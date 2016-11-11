3e79f67a95f8950baacde7b923594f56e3899a16
Somkala <somkala.pachhai@itnepal.com>
2016-11-11 12:56:15 +0545

    new file

---
+++ module/Application/src/Controller/CronController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Application\Controller;
+
+use Application\Helper\Helper;
+use AttendanceManagement\Model\Attendance;
+use AttendanceManagement\Model\AttendanceByHr;
+use AttendanceManagement\Repository\AttendanceByHrRepository;
+use HolidayManagement\Model\Holiday;
+use HolidayManagement\Repository\HolidayRepository;
+use LeaveManagement\Model\LeaveApply;
+use SelfService\Repository\LeaveRequestRepository;
+use Setup\Model\HrEmployees;
+use Setup\Repository\EmployeeRepository;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class CronController extends AbstractActionController {
+
+    private $adapter;
+    private $date;
+
+    public function __construct(AdapterInterface $adapter) {
+        $this->adapter = $adapter;
+        $this->date = Helper::getcurrentExpressionDate();
+    }
+
+    public function indexAction() {
+
+        $employeeList = $this->pullEmployeeList();
+        $attendanceRepo = new AttendanceByHrRepository($this->adapter);
+        foreach ($employeeList as $employee) {
+            $attendance = new Attendance();
+            $attendance->employeeId = $employee->employeeId;
+            $attendance->attendanceDt = Helper::getcurrentExpressionDate();
+            $attendanceRepo->addAttendance($attendance);
+
+            $attendanceDetail = new AttendanceByHr();
+            $attendanceDetail->attendanceDt = $attendance->attendanceDt;
+            $attendanceDetail->employeeId = $attendance->employeeId;
+            $attendanceDetail->id = ((int) Helper::getMaxId($this->adapter, AttendanceByHr::TABLE_NAME, AttendanceByHr::ID)) + 1;
+
+            $checkForHoliday = $this->checkForHoliday($employee, $this->date);
+            if ($checkForHoliday == null) {
+                $checkForleave = $this->checkForLeave($employee, $this->date);
+                if ($checkForleave == null) {
+                    $attendanceRepo->add($attendanceDetail);
+                } else {
+                    $attendanceDetail->leaveId = $checkForleave[LeaveApply::LEAVE_ID];
+                    $attendanceRepo->add($attendanceDetail);
+                }
+            } else {
+                echo $checkForHoliday[Holiday::HOLIDAY_ID];
+                $attendanceDetail->holidayId = $checkForHoliday[Holiday::HOLIDAY_ID];
+                $attendanceRepo->add($attendanceDetail);
+            }
+        }
+
+        return [];
+    }
+
+    public function employeeAttendanceAction() {
+        
+    }
+
+    private function pullEmployeeList() {
+        $employeeRepo = new EmployeeRepository($this->adapter);
+        return $employeeRepo->fetchAll();
+    }
+
+    private function checkForHoliday(HrEmployees $employee, $date) {
+        $holidayRepo = new HolidayRepository($this->adapter);
+        return $holidayRepo->checkEmployeeOnHoliday($date, $employee->branchId, $employee->genderId);
+    }
+
+    private function checkForLeave(HrEmployees $employee, $date) {
+        $leaveRepo = new LeaveRequestRepository($this->adapter);
+        return $leaveRepo->checkEmployeeLeave($employee->employeeId, $date);
+    }
+
+}
