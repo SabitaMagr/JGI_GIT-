e44e4947d49bc7028919c71e3eb93d220260a22e
ukesh <ukesh2010@gmail.com>
2016-11-25 15:50:07 +0545

    kendo grid issue fixed in employee list

--- public/js/setup/employee/employeeListController.js
+++ public/js/setup/employee/employeeListController.js
@@ -1,28 +1,28 @@
-angular.module('hris', ["kendo.directives"])
+//angular.module('hris', ["kendo.directives"])
+angular.module('hris', [])
         .controller('employeeListController', function ($scope, $http) {
-            $scope.gridData = new kendo.data.ObservableArray([
-            ]);
-            $scope.gridColumns = [
-                {field: "employeeCode", title: "Employee Code"},
-                {field: "firstName", title: "Name"},
-                {field: "birthDate", title: "Birth Date"},
-                {field: "mobileNo", title: "Mobile No"},
-                {field: "emailOfficial", title: "Email Official"},
-                {title: "Action"}
-            ];
-            $scope.test = {
-                height: 550,
-                scrollable: true,
-                sortable: true,
-                filterable: true,
-                rowTemplate: kendo.template($("#rowTemplate").html()),
-                pageable: {
-                    input: true,
-                    numeric: false
-                }
-            };
+//            $scope.gridData = new kendo.data.ObservableArray([
+//            ]);
+//            $scope.gridColumns = [
+//                {field: "employeeCode", title: "Employee Code"},
+//                {field: "firstName", title: "Name"},
+//                {field: "birthDate", title: "Birth Date"},
+//                {field: "mobileNo", title: "Mobile No"},
+//                {field: "emailOfficial", title: "Email Official"},
+//                {title: "Action"}
+//            ];
+//            $scope.kendoGridOptions = {
+//                height: 550,
+//                scrollable: true,
+//                sortable: true,
+//                filterable: true,
+//                rowTemplate: kendo.template($("#rowTemplate").html()),
+//                pageable: {
+//                    input: true,
+//                    numeric: false
+//                },
+//            };
             $scope.view = function () {
-
                 var employeeId = angular.element(document.getElementById('employeeId')).val();
                 var branchId = angular.element(document.getElementById('branchId')).val();
                 var departmentId = angular.element(document.getElementById('departmentId')).val();
@@ -43,15 +43,43 @@ angular.module('hris', ["kendo.directives"])
                         'serviceEventTypeId': serviceEventTypeId
                     }
                 }).then(function (success) {
+                    $scope.initializekendoGrid(success.data);
                     $scope.$apply(function () {
-                        $scope.gridData.splice(0, $scope.gridData.length);
-                        angular.forEach(success.data, function (value, key) {
-                            $scope.gridData.push(value);
-                        });
+//                        $scope.gridData.splice(0, $scope.gridData.length);
+//                        angular.forEach(success.data, function (value, key) {
+//                            $scope.gridData.push(value);
+//                        });
+
                     });
                 }, function (failure) {
                     console.log(failure);
                 });
-            }
+            };
+
+            $scope.initializekendoGrid = function (employees) {
+                $("#employeeTable").kendoGrid({
+                    dataSource: {
+                        data: employees,
+                        pageSize: 20
+                    },
+                    height: 450,
+                    scrollable: true,
+                    sortable: true,
+                    filterable: true,
+                    pageable: {
+                        input: true,
+                        numeric: false
+                    },
+                    rowTemplate: kendo.template($("#rowTemplate").html()),
+                    columns: [
+                        {field: "employeeCode", title: "Employee Code"},
+                        {field: "firstName", title: "Name"},
+                        {field: "birthDate", title: "Birth Date"},
+                        {field: "mobileNo", title: "Mobile No"},
+                        {field: "emailOfficial", title: "Email Official"},
+                        {title: "Action"}
+                    ]
+                });
+            };
         });
 
