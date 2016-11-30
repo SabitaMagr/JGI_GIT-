be7064a8626c71531bec1a278cb8e5e96e75ce06
ukesh <ukesh2010@gmail.com>
2016-11-30 12:12:18 +0545

    employee edit changes

--- public/js/form_wizard.js
+++ public/js/form_wizard.js
@@ -3,8 +3,8 @@
     $(document).ready(function () {
         $('#rootwizard').bootstrapWizard({
             onTabShow: function (tab, navigation, index) {
-                $('#tab' + (index + 1) + " select").select2();
 
+                $('#tab' + (index + 1) + " select").select2();
                 switch (index + 1) {
                     case 1:
                         window.app.addDatePicker($("#employeeBirthDate"));
