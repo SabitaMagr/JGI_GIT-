5fa00552979300d82306cfafb586a55163847a5b
ukesh <ukesh2010@gmail.com>
2017-01-16 17:27:02 +0545

    changes

--- module/Notification/src/Controller/EmailController.php
+++ module/Notification/src/Controller/EmailController.php
@@ -2,6 +2,7 @@
 
 namespace Notification\Controller;
 
+use Application\Helper\Helper;
 use Zend\Authentication\AuthenticationService;
 use Zend\Db\Adapter\AdapterInterface;
 use Zend\Mvc\Controller\AbstractActionController;
@@ -11,13 +12,27 @@ class EmailController extends AbstractActionController {
     private $employeeId;
     private $adapter;
 
+    const EMAIL_TYPES = [
+        1 => "TYPE_ONE",
+        2 => "TYPE_TWO",
+        3 => "TYPE_THREE"];
+
     public function __construct(AdapterInterface $adapter) {
         $this->adapter = $adapter;
 
         $auth = new AuthenticationService();
         $this->employeeId = $auth->getStorage()->read()['employee_id'];
     }
-    
-    
-    
+
+    public function indexAction() {
+        return Helper::addFlashMessagesToArray($this, ['emailTypes' => self::EMAIL_TYPES]);
+    }
+
+    public function editAction() {
+        $request = $this->getRequest();
+        print "<pre>";
+        print_r($request->getPost());
+        exit;
+    }
+
 }
