62211fb5e83383b5e7980bea6564f826c2de5a11
ukesh <alcute56@gmail.com>
2017-01-25 13:55:07 +0545

    added

--- module/WorkOnDayoff/src/Controller/Apply.php
+++ module/WorkOnDayoff/src/Controller/Apply.php
@@ -1,14 +1,207 @@
 <?php
 namespace WorkOnDayoff\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use Application\Helper\Helper;
+use Application\Helper\EntityHelper;
+use WorkOnDayoff\Repository\WorkOnDayoffStatusRepository;
+use ManagerService\Repository\DayoffWorkApproveRepository;
+use SelfService\Repository\WorkOnDayoffRepository;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Mvc\Controller\AbstractActionController;
+use SelfService\Form\WorkOnDayoffForm;
+use Zend\Form\Annotation\AnnotationBuilder;
+use SelfService\Model\WorkOnDayoff;
+use Setup\Model\Branch;
+use Setup\Model\Department;
+use Setup\Model\Designation;
+use Setup\Model\Position;
+use Setup\Model\ServiceType;
+use Zend\Form\Element\Select;
+use Setup\Model\ServiceEventType;
+use Zend\Authentication\AuthenticationService;
+use Setup\Repository\RecommendApproveRepository;
 
-class Apply extends AbstractActionController{
-    public function __construct(){
-        
+class WorkOnDayoffStatus extends AbstractActionController
+{
+    private $adapter;
+    private $dayoffWorkApproveRepository;
+    private $workonDayoffStatusRepository;
+    private $form;
+    private $employeeId;
+    
+    public function __construct(AdapterInterface $adapter) {
+        $this->adapter = $adapter;
+        $this->dayoffWorkApproveRepository = new DayoffWorkApproveRepository($adapter);
+        $this->workonDayoffStatusRepository = new WorkOnDayoffStatusRepository($adapter);
+        $auth = new AuthenticationService();
+        $this->employeeId =  $auth->getStorage()->read()['employee_id'];       
     }
+    
+    public function initializeForm(){
+        $builder = new AnnotationBuilder();
+        $form = new WorkOnDayoffForm();
+        $this->form = $builder->createForm($form);
+    }
+
+    
     public function indexAction() {
-        return Helper::addFlashMessagesToArray($this, ['list'=>'hellow day off apply']);
+        $employeeNameFormElement = new Select();
+        $employeeNameFormElement->setName("branch");
+        $employeeName = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, "HR_EMPLOYEES", "EMPLOYEE_ID", ["FIRST_NAME", "MIDDLE_NAME", "LAST_NAME"], ["STATUS" => "E"], "FIRST_NAME", "ASC", " ");
+        $employeeName1 = [-1 => "All"] + $employeeName;
+        $employeeNameFormElement->setValueOptions($employeeName1);
+        $employeeNameFormElement->setAttributes(["id" => "employeeId", "class" => "form-control"]);
+        $employeeNameFormElement->setLabel("Employee");
+        $employeeNameFormElement->setAttribute("ng-click", "view()");
+
+        $branchFormElement = new Select();
+        $branchFormElement->setName("branch");
+        $branches = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, Branch::TABLE_NAME, Branch::BRANCH_ID, [Branch::BRANCH_NAME], [Branch::STATUS => 'E'], "BRANCH_NAME", "ASC");
+        $branches1 = [-1 => "All"] + $branches;
+        $branchFormElement->setValueOptions($branches1);
+        $branchFormElement->setAttributes(["id" => "branchId", "class" => "form-control"]);
+        $branchFormElement->setLabel("Branch");
+        $branchFormElement->setAttribute("ng-click", "view()");
+
+        $departmentFormElement = new Select();
+        $departmentFormElement->setName("department");
+        $departments = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, Department::TABLE_NAME, Department::DEPARTMENT_ID, [Department::DEPARTMENT_NAME], [Department::STATUS => 'E'], "DEPARTMENT_NAME", "ASC");
+        $departments1 = [-1 => "All"] + $departments;
+        $departmentFormElement->setValueOptions($departments1);
+        $departmentFormElement->setAttributes(["id" => "departmentId", "class" => "form-control"]);
+        $departmentFormElement->setLabel("Department");
+
+        $designationFormElement = new Select();
+        $designationFormElement->setName("designation");
+        $designations = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, Designation::TABLE_NAME, Designation::DESIGNATION_ID, [Designation::DESIGNATION_TITLE], [Designation::STATUS => 'E'], "DESIGNATION_TITLE", "ASC");
+        $designations1 = [-1 => "All"] + $designations;
+        $designationFormElement->setValueOptions($designations1);
+        $designationFormElement->setAttributes(["id" => "designationId", "class" => "form-control"]);
+        $designationFormElement->setLabel("Designation");
+
+        $positionFormElement = new Select();
+        $positionFormElement->setName("position");
+        $positions = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, Position::TABLE_NAME, Position::POSITION_ID, [Position::POSITION_NAME], [Position::STATUS => 'E'], "POSITION_NAME", "ASC");
+        $positions1 = [-1 => "All"] + $positions;
+        $positionFormElement->setValueOptions($positions1);
+        $positionFormElement->setAttributes(["id" => "positionId", "class" => "form-control"]);
+        $positionFormElement->setLabel("Position");
+
+        $serviceTypeFormElement = new Select();
+        $serviceTypeFormElement->setName("serviceType");
+        $serviceTypes = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, ServiceType::TABLE_NAME, ServiceType::SERVICE_TYPE_ID, [ServiceType::SERVICE_TYPE_NAME], [ServiceType::STATUS => 'E'], "SERVICE_TYPE_NAME", "ASC");
+        $serviceTypes1 = [-1 => "All"] + $serviceTypes;
+        $serviceTypeFormElement->setValueOptions($serviceTypes1);
+        $serviceTypeFormElement->setAttributes(["id" => "serviceTypeId", "class" => "form-control"]);
+        $serviceTypeFormElement->setLabel("Service Type");
+        
+        $serviceEventTypeFormElement = new Select();
+        $serviceEventTypeFormElement->setName("serviceEventType");
+        $serviceEventTypes = \Application\Helper\EntityHelper::getTableKVListWithSortOption($this->adapter, ServiceEventType::TABLE_NAME, ServiceEventType::SERVICE_EVENT_TYPE_ID, [ServiceEventType::SERVICE_EVENT_TYPE_NAME], [ServiceEventType::STATUS => 'E'], "SERVICE_EVENT_TYPE_NAME", "ASC");
+        $serviceEventTypes1 = [-1 => "Working"] + $serviceEventTypes;
+        $serviceEventTypeFormElement->setValueOptions($serviceEventTypes1);
+        $serviceEventTypeFormElement->setAttributes(["id" => "serviceEventTypeId", "class" => "form-control"]);
+        $serviceEventTypeFormElement->setLabel("Service Event Type");
+
+        $status = [
+            '-1' => 'All',
+            'RQ' => 'Pending',
+            'RC' => 'Recommended',
+            'AP' => 'Approved',
+            'R' => 'Rejected',
+            'C'=>'Cancelled'
+        ];
+        $statusFormElement = new Select();
+        $statusFormElement->setName("status");
+        $statusFormElement->setValueOptions($status);
+        $statusFormElement->setAttributes(["id" => "requestStatusId", "class" => "form-control"]);
+        $statusFormElement->setLabel("Status");
+
+        return Helper::addFlashMessagesToArray($this, [
+                    "branches" => $branchFormElement,
+                    "departments" => $departmentFormElement,
+                    'designations' => $designationFormElement,
+                    'positions' => $positionFormElement,
+                    'serviceTypes' => $serviceTypeFormElement,
+                    'employees' => $employeeNameFormElement,
+                    'status' => $statusFormElement,
+                    'serviceEventTypes' => $serviceEventTypeFormElement
+        ]);
     }
+    public function viewAction() {
+        $this->initializeForm();
+        $dayoffWorkApproveRepository = new LoanApproveRepository($this->adapter);
+
+        $id = (int) $this->params()->fromRoute('id');
+
+        if ($id === 0) {
+            return $this->redirect()->toRoute("loanStatus");
+        }
+        $loanRequest = new LoanRequest();
+        $request = $this->getRequest();
+
+        $detail = $this->loanApproveRepository->fetchById($id);
+        $status = $detail['STATUS'];
+        $employeeId = $detail['EMPLOYEE_ID'];
+        $approvedDT = $detail['APPROVED_DATE'];
+
+        $requestedEmployeeID = $detail['EMPLOYEE_ID'];
+        $recommendApproveRepository = new RecommendApproveRepository($this->adapter);
+        $empRecommendApprove = $recommendApproveRepository->fetchById($requestedEmployeeID);
+        $recommApprove = 0;
+        if($empRecommendApprove['RECOMMEND_BY']==$empRecommendApprove['APPROVED_BY']){
+            $recommApprove=1;
+        }
+        
+        $employeeName = $detail['FIRST_NAME'] . " " . $detail['MIDDLE_NAME'] . " " . $detail['LAST_NAME'];        
+        $RECM_MN = ($detail['RECM_MN']!=null)? " ".$detail['RECM_MN']." ":" ";
+        $recommender = $detail['RECM_FN'].$RECM_MN.$detail['RECM_LN'];        
+        $APRV_MN = ($detail['APRV_MN']!=null)? " ".$detail['APRV_MN']." ":" ";
+        $approver = $detail['APRV_FN'].$APRV_MN.$detail['APRV_LN'];
+        $MN1 = ($detail['MN1']!=null)? " ".$detail['MN1']." ":" ";
+        $recommended_by = $detail['FN1'].$MN1.$detail['LN1'];        
+        $MN2 = ($detail['MN2']!=null)? " ".$detail['MN2']." ":" ";
+        $approved_by = $detail['FN2'].$MN2.$detail['LN2'];
+        $authRecommender = ($status=='RQ' || $status=='C')?$recommender:$recommended_by;
+        $authApprover = ($status=='RC' || $status=='C' || $status=='RQ' || ($status=='R' && $approvedDT==null))?$approver:$approved_by;
+
+
+        if (!$request->isPost()) {
+            $loanRequest->exchangeArrayFromDB($detail);
+            $this->form->bind($loanRequest);
+        } else {
+            $getData = $request->getPost();
+            $reason = $getData->approvedRemarks;
+            $action = $getData->submit;
+
+            $loanRequest->approvedDate = Helper::getcurrentExpressionDate();
+            if ($action == "Reject") {
+                $loanRequest->status = "R";
+                $this->flashmessenger()->addMessage("Loan Request Rejected!!!");
+            } else if ($action == "Approve") {
+                $loanRequest->status = "AP";
+                $this->flashmessenger()->addMessage("Loan Request Approved");
+            }
+            $loanRequest->approvedBy = $this->employeeId;
+            $loanRequest->approvedRemarks = $reason;
+            $this->loanApproveRepository->edit($loanRequest, $id);
+
+            return $this->redirect()->toRoute("loanStatus");
+        }
+        return Helper::addFlashMessagesToArray($this, [
+                    'form' => $this->form,
+                    'id' => $id,
+                    'employeeId' => $employeeId,
+                    'employeeName' => $employeeName,
+                    'requestedDt' => $detail['REQUESTED_DATE'],
+                    'recommender' => $authRecommender,
+                    'approvedDT'=>$detail['APPROVED_DATE'],
+                    'approver' => $authApprover,
+                    'status' => $status,
+                    'loans' => EntityHelper::getTableKVListWithSortOption($this->adapter, Loan::TABLE_NAME, Loan::LOAN_ID, [Loan::LOAN_NAME], [Loan::STATUS => "E"], Loan::LOAN_ID, "ASC"),
+                    'customRenderer' => Helper::renderCustomView(),
+                    'recommApprove'=>$recommApprove
+        ]);
+    }
+    
 }
