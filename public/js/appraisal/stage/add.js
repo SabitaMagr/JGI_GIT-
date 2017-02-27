5c3654fc8f226f71a2fcc9ab8d0a8f661f25eb42
ukesh <alcute56@gmail.com>
2017-02-27 11:17:41 +0545

    updated

---
+++ public/js/appraisal/stage/add.js
@@ -0,0 +1,23 @@
+(function ($,app) {
+    'use strict';
+    $(document).ready(function () {
+        $('select').select2();
+        app.startEndDatePicker('startDate', 'endDate');
+        
+        var inputFieldId = "stageEdesc";
+        var formId = "appraisalStage-form";
+        var tableName =  "HR_APPRAISAL_STAGE";
+        var columnName = "STAGE_EDESC";
+        var checkColumnName = "STAGE_ID";
+        var selfId = $("#stageId").val();
+        if (typeof(selfId) == "undefined"){
+            selfId=0;
+        }
+        window.app.checkUniqueConstraints(inputFieldId,formId,tableName,columnName,checkColumnName,selfId);       
+        window.app.checkUniqueConstraints("stageCode",formId,tableName,"STAGE_CODE",checkColumnName,selfId);
+        window.app.checkUniqueConstraints("stageNdesc",formId,tableName,"STAGE_NDESC",checkColumnName,selfId);
+        window.app.checkUniqueConstraints("orderNo",formId,tableName,"ORDER_NO",checkColumnName,selfId);
+
+    });
+})(window.jQuery,window.app);
+
---
+++ public/js/appraisal/stage/index.js
@@ -0,0 +1,36 @@
+(function ($) {
+    'use strict';
+    $(document).ready(function () {
+        $("#appraisalStageTable").kendoGrid({
+            excel: {
+                fileName: "AppraisalStageList.xlsx",
+                filterable: true,
+                allPages: true
+            },
+            dataSource: {
+                data: document.stages,
+                page: 1,
+            },
+            height: 450,
+            scrollable: true,
+            sortable: true,
+            filterable: true,
+            pageable: true,
+            rowTemplate: kendo.template($("#rowTemplate").html()),
+            columns: [
+                {field: "STAGE_CODE", title: "Stage Code",width:150},
+                {field: "STAGE_EDESC", title: "Stage Ename",width:180},
+                {field: "STAGE_NDESC", title: "Stage Nname",width:180},
+                {field: "ORDER_NO", title: "Order No.",width:180},
+                {field: "START_DATE", title: "Start Date",width:180},
+                {field: "END_DATE", title: "End Date",width:180},
+                {title: "Action",width:100}
+            ],
+        });
+        $("#export").click(function (e) {
+            var grid = $("#appraisalStageTable").data("kendoGrid");
+            grid.saveAsExcel();
+        });
+        window.app.UIConfirmations();
+    });
+})(window.jQuery);
