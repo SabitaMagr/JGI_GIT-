ade71a5352c931fd956cabaa54142d0e553ffc8e
punam <alcute56@gmail.com>
2017-03-20 12:15:44 +0545

    loan request bug fixed out

--- public/js/SelfService/loan-request/add.js
+++ public/js/SelfService/loan-request/add.js
@@ -4,7 +4,7 @@
         $('select').select2();
         app.addDatePicker($("#startDate"));
         /* prevent past event post */
-        $('#startDate').datepicker("setStartDate", new Date());
+        //$('#startDate').datepicker("setStartDate", new Date());
         /* end of  prevent past event post */
     });
 })(window.jQuery, window.app);
