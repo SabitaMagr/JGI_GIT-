a911b038be697e880d37dda75e509982e970c9da
Himal Rai <himal.rai@itnepal.com>
2017-03-24 12:21:37 +0545

    commit before modern slipper

---
+++ module/System/src/Model/AttendanceDevice.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace System\Model;
+
+use Application\Model\Model;
+
+class AttendanceDevice extends Model {
+
+    const DEVICE_ID = "DEVICE_ID";
+    const DEVICE_NAME = "DEVICE_NAME";
+    const DEVICE_IP = "DEVICE_IP";
+    const DEVICE_LOCATION = "DEVICE_LOCATION";
+    const ISACTIVE = "ISACTIVE";
+    const COMPANY_ID = "COMPANY_ID";
+    const BRANCH_ID = "BRANCH_ID";
+
+    public $deviceId;
+    public $deviceName;
+    public $deviceIp;
+    public $deviceLocation;
+    public $isactive;
+    public $companyId;
+    public $branchId;
+    public $mappings = [
+        'deviceId' => self::DEVICE_ID,
+        'deviceName' => self::DEVICE_NAME,
+        'deviceIp' => self::DEVICE_IP,
+        'deviceLocation' => self::DEVICE_LOCATION,
+        'isactive' => self::ISACTIVE,
+        'companyId' => self::COMPANY_ID,
+        'branchId' => self::BRANCH_ID,
+    ];
+
+}
