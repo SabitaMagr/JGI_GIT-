da4104223cad79179f50b71bdbe525af423bb191
Himal Rai <himal.rai@itnepal.com>
2017-04-04 16:52:03 +0545

    changes

--- module/System/src/Model/AttendanceDevice.php
+++ module/System/src/Model/AttendanceDevice.php
@@ -6,8 +6,7 @@ use Application\Model\Model;
 
 class AttendanceDevice extends Model {
 
-
-    const TABLE_NAME = "HRIS_ATTENDANCE_DEVICE";
+    const TABLE_NAME = "HRIS_ATTD_DEVICE_MASTER";
     const DEVICE_ID = "DEVICE_ID";
     const DEVICE_NAME = "DEVICE_NAME";
     const DEVICE_IP = "DEVICE_IP";
@@ -16,7 +15,7 @@ class AttendanceDevice extends Model {
     const COMPANY_ID = "COMPANY_ID";
     const BRANCH_ID = "BRANCH_ID";
     const STATUS = "STATUS";
-    const DEVICE_COMPANY="DEVICE_COMPANY";
+    const DEVICE_COMPANY = "DEVICE_COMPANY";
 
     public $deviceId;
     public $deviceName;
@@ -27,7 +26,6 @@ class AttendanceDevice extends Model {
     public $branchId;
     public $status;
     public $deviceCompany;
-
     public $mappings = [
         'deviceId' => self::DEVICE_ID,
         'deviceName' => self::DEVICE_NAME,
