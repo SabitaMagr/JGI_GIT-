a2aa9f78371272280cc6804ecc5bc9a184ccbee0
Himal Rai <himal.rai@itnepal.com>
2017-04-19 17:25:15 +0545

    ukesh sir changes

---
+++ public/js/dashboard/todo.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var itemVal = document.todoTaskList;
+    $('#lobilist').lobiList({
+        lists: [
+            {
+                id: 'lob',
+                title: 'TODO',
+                defaultStyle: 'lobilist-success',
+                controls: [],
+                useCheckboxes: true,
+                items: itemVal,
+
+            }
+        ],
+        actions: {
+            'update': document.todoUpdateUrl,
+            'insert': document.todoAddUrl,
+            'delete': document.todoDeleteUrl
+        },
+        afterMarkAsDone: function ($list, $object) {
+            updateStatus($object);
+        },
+        afterMarkAsUndone: function ($list, $object) {
+            updateStatus($object);
+        },
+
+    });
+
+    var $list = $('#lob').data('lobiList');
+    var $dueDateInput = $list.$el.find('form [name=dueDate]');
+    window.app.addDatePicker($dueDateInput);
+
+
+    updateStatus = function ($data) {
+        $.ajax({
+            type: 'POST',
+            url: document.todoTaskUpdateStatus,
+            data: {'item': $data},
+        }).done(function (res) {
+        });
+    };
+});
