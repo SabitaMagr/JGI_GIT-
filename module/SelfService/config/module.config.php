94bdd58f791f3bfd54e1e63c793fc20d26cc6658
punam <alcute56@gmail.com>
2017-05-01 16:35:05 +0545

    updated

--- module/SelfService/config/module.config.php
+++ module/SelfService/config/module.config.php
@@ -27,6 +27,7 @@ use SelfService\Controller\PerformanceAppraisal;
 use SelfService\Controller\LeaveNotification;
 use SelfService\Controller\TravelNotification;
 use SelfService\Controller\TrainingRequest;
+use SelfService\Controller\OvertimeRequest;
 
 return [
     'router' => [
@@ -280,6 +281,20 @@ return [
                     ],
                     'defaults' => [
                         'controller' => TrainingRequest::class,
+                        'action' => 'index',
+                    ]
+                ],
+            ],
+            'overtimeRequest' => [
+                'type' => Segment::class,
+                'options' => [
+                    'route' => '/selfservice/overtimeRequest[/:action[/:id]]',
+                    'constants' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => OvertimeRequest::class,
                         'action' => 'index',
                     ]
                 ],
@@ -819,7 +834,8 @@ return [
             PerformanceAppraisal::class=> ControllerFactory::class,
             LeaveNotification::class=> ControllerFactory::class,
             TravelNotification::class=> ControllerFactory::class,
-            TrainingRequest::class => ControllerFactory::class
+            TrainingRequest::class => ControllerFactory::class,
+            OvertimeRequest::class => ControllerFactory::class
         ],
     ],
     'view_manager' => [
---
+++ module/SelfService/src/Model/Overtime.php
@@ -0,0 +1,55 @@
+<?php
+namespace SelfService\Model;
+
+use Application\Model\Model;
+
+class Overtime extends Model{
+    const TABLE_NAME ="HRIS_OVERTIME";
+    
+    const OVERTIME_ID = "OVERTIME_ID";
+    const EMPLOYEE_ID = "EMPLOYEE_ID";
+    const OVERTIME_DATE = "OVERTIME_DATE";
+    const REQUESTED_DATE = "REQUESTED_DATE";
+    const DESCRIPTION = "DESCRIPTION";
+    const REMARKS = "REMARKS";
+    const STATUS = "STATUS";
+    const RECOMMENDED_BY = "RECOMMENDED_BY";
+    const RECOMMENDED_DATE = "RECOMMENDED_DATE";
+    const RECOMMENDED_REMARKS = "RECOMMENDED_REMARKS";
+    const APPROVED_BY ="APPROVED_BY";
+    const APPROVED_DATE = "APPROVED_DATE";
+    const APPROVED_REMARKS = "APPROVED_REMARKS";
+    const MODIFIED_DATE = "MODIFIED_DATE";
+    
+    public $overtimeId;
+    public $employeeId;
+    public $overtimeDate;
+    public $requestedDate;
+    public $description;
+    public $remarks;
+    public $status;
+    public $recommendedBy;
+    public $recommendedDate;
+    public $recommendedRemarks;
+    public $approvedBy;
+    public $approvedDate;
+    public $approvedRemarks;
+    public $modifiedDate;
+    
+    public $mappings = [
+        'overtimeId'=>self::OVERTIME_ID,
+        'employeeId'=>self::EMPLOYEE_ID,
+        'overtimeDate'=>self::OVERTIME_DATE,
+        'requestedDate'=>self::REQUESTED_DATE,
+        'description'=>self::DESCRIPTION,
+        'remarks'=>self::REMARKS,
+        'status'=>self::STATUS,
+        'recommendedBy'=>self::RECOMMENDED_BY,
+        'recommendedDate'=>self::RECOMMENDED_DATE,
+        'recommendedRemarks'=>self::RECOMMENDED_REMARKS,
+        'approvedBy'=>self::APPROVED_BY,
+        'approvedDate'=>self::APPROVED_DATE,
+        'approvedRemarks'=>self::APPROVED_REMARKS,
+        'modifiedDate'=>self::MODIFIED_DATE
+    ];
+}
