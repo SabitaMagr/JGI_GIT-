cbbb31e87691caa92a87723f191911f6d4b738db
prabin <prabin.maharjan@itnepal.com>
2017-05-01 17:43:28 +0545

    commit

--- public/js/manager-service/salary-review/index.js
+++ public/js/manager-service/salary-review/index.js
@@ -18,10 +18,10 @@
             pageable: true,
             rowTemplate: kendo.template($("#rowTemplate").html()),
             columns: [
-                {field: "FIRST_NAME", title: "Employee", width: 100},
-                {field: "OLD_AMOUNT", title: "Old Amount", width: 200},
-                {field: "NEW_AMOUNT", title: "New Amount", width: 180},
-                {field: "EFFECTIVE_DATE", title: "Effective Date", width: 100},
+                {field: "FIRST_NAME", title: "Employee", width: 200},
+                {field: "OLD_AMOUNT", title: "Old Amount", width: 120},
+                {field: "NEW_AMOUNT", title: "New Amount", width: 120},
+                {field: "EFFECTIVE_DATE", title: "Effective Date", width: 120},
                 {field: "JOB_HISTORY_ID", title: "JOb History Id", width: 140},
                 {title: "Action", width: 100}
             ],
