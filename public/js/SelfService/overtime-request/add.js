4a4fa9a22d3932296b38d35adec8d0043cdcdd3b
punam <alcute56@gmail.com>
2017-05-02 12:51:09 +0545

    updated

--- public/js/SelfService/overtime-request/add.js
+++ public/js/SelfService/overtime-request/add.js
@@ -5,14 +5,6 @@
     'use strict';
     $(document).ready(function () {
 
-        $('select').select2();
-        $('#startTime').combodate({
-            minuteStep: 1
-        });
-        $('#endTime').combodate({
-            minuteStep: 1
-        });
-
         var $overtimeDate = $("#overtimeDate");
         if (!($overtimeDate.is('[readonly]'))) {
             app.datePickerWithNepali("overtimeDate", "nepaliDate");
@@ -24,21 +16,6 @@
         } else {
             app.datePickerWithNepali("overtimeDate", "nepaliDate");
         }
-
-        var totalHour = function () {
-            var tim_i = new Date("01/01/2007 " + $('#startTime').val());
-            var tim_o = new Date("01/01/2007 " + $('#endTime').val());
-
-            var diff1 = (tim_i - tim_o) / 60000; //dividing by seconds and milliseconds
-            var diff = Math.abs(diff1);
-            var minutes = diff % 60;
-            var hours = (diff - minutes) / 60;
-
-            var total_tim = hours + '.' + minutes;
-            $("#totalHour").val(total_tim);
-        };
-        $("#startTime").on("change", totalHour);
-        $("#endTime").on("change", totalHour);
 
         var $employeeId = $('#employeeId');
         app.floatingProfile.setDataFromRemote($employeeId.val());
