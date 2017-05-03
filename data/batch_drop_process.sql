b88c04105028addb2630422070f6bcae4ce984f6
Himal Rai <himal.rai@itnepal.com>
2017-05-03 17:33:15 +0545

    database backup process

--- data/batch_drop_process.sql
+++ data/batch_drop_process.sql
@@ -3,7 +3,7 @@ BEGIN
   (SELECT object_name,
     object_type
   FROM user_objects
-  WHERE object_type IN ( 'VIEW' )
+  WHERE object_type IN ( 'VIEW','TABLE','FUNCTION','PROCEDURE')
   )
   LOOP
     BEGIN
