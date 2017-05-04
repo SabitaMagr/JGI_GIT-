2766ef84a6b30973011c4b1dda1157a80d33550a
prabin <prabin.maharjan@itnepal.com>
2017-05-04 14:50:39 +0545

    commit

---
+++ public/js/asset/issue/index.js
@@ -0,0 +1,38 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        console.log(document.issue);
+        $("#assetIssueTable").kendoGrid({
+            excel: {
+                fileName: "AssetIssueList.xlsx",
+                filterable: true,
+                allPages: true
+            },
+            dataSource: {
+                data: document.issue,
+                page: 1,
+            },
+            height: 450,
+            scrollable: true,
+            sortable: true,
+            filterable: true,
+            pageable: true,
+            rowTemplate: kendo.template($("#rowTemplate").html()),
+            columns: [
+                {field: "ASSET_EDESC", title: "Asset", width: 120},
+                {field: "FIRST_NAME", title: "Employee ", width: 140},
+                {field: "ISSUE_DATE", title: "Issue Date", width: 140},
+                {field: "QUANTITY", title: "Quantity ", width: 140},
+                {field: "RETURNED_DATE", title: "ReturnDate", width: 130},
+                {title: "Action", width: 120}
+            ],
+        });
+        $("#export").click(function (e) {
+            var grid = $("#assetIssueTable").data("kendoGrid");
+            grid.saveAsExcel();
+        });
+        window.app.UIConfirmations();
+    });
+
+})(window.jQuery);
