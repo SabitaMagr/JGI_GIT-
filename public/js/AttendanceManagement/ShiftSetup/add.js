34f6f2b12ffcce74cd154757c4c4d7bda961bc4c
punam <alcute56@gmail.com>
2017-05-09 10:25:47 +0545

    updated

--- public/js/AttendanceManagement/ShiftSetup/add.js
+++ public/js/AttendanceManagement/ShiftSetup/add.js
@@ -1,6 +1,7 @@
 (function ($, app) {
     'use strict';
     $(document).ready(function () {
+        $("select").select2();
         $('#startTime').combodate({
             minuteStep: 1
         });
