deac817f2e964c10a77412bc3cdbd7fb0e0e9460
punam <alcute56@gmail.com>
2017-06-15 15:13:37 +0545

    updated

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,8 +10,8 @@ return [
         )
         )',
         
-        'username' => 'HRIS',
-        'password' => 'NEO_HRIS',
+        'username' => 'LAXMI_HRIS_APR28',
+        'password' => 'LAXMI_HRIS_APR28',
         
 //          'username' => 'HRIS_DEMO',
 //          'password' => 'HRIS_DEMO',
