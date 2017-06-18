ec4b3a689260b3cd85528748b16406c38cc8e616
prabin <prabin.maharjan@itnepal.com>
2017-06-18 15:30:31 +0545

    commit

--- public/js/asset/setup/setupController.js
+++ public/js/asset/setup/setupController.js
@@ -3,14 +3,14 @@
 
 
     $(document).ready(function () {
-        
+
 
         app.addDatePicker($('#issueDate'));
         app.addDatePicker($('#requestDate'));
         app.addDatePicker($('#returnDate'));
-        
-        $("form").submit(function() {
-          App.blockUI({target: "form"});
+
+        $("form").submit(function () {
+            App.blockUI({target: "form"});
         });
 
     });
@@ -18,41 +18,50 @@
 })(window.jQuery, window.app);
 
 
-angular.module('hris', [])
-        .controller('setupController', function ($scope, $http) {
+angular.module('hris', ['ui.bootstrap'])
+        .controller('setupController', function ($scope, $http, $uibModal) {
+            
+            $scope.assetNameView = 'asdasadas';
 
+//            $('select').select2();
+            
+            $scope.asset = '1';
+                $scope.assetNameView = 'lenovo';
 
             $scope.assetIssue = function (assetname, assetId) {
 //                console.log(assetname);
 //                console.log(assetId);
-                $scope.asset = assetId;
+//                $scope.asset = assetId;
                 $scope.assetNameView = assetname;
-
-                window.app.pullDataById(document.restfulUrl, {
-                    action: 'pullAssetBalance',
-                    data: {
-                        assetId: $scope.asset
-                    }
-
-                }).then(function (success) {
-                    $scope.$apply(function () {
-                        if(success.data==null||success.data==0||success.data=='undefined'){
-                        $('#IssueSubmitBtn').attr('disabled','disabled');
-                        }else{
-                            $('#IssueSubmitBtn').attr('disabled',false);
-                        }
-                        $scope.rQ = 'REM BALANCE: ' + success.data;
-                        $scope.bal = success.data;
-                        $("#quantity").attr({"max": success.data, "min": 1});
-                    });
-                }, function (error) {
-                    console.log("error", error);
-                })
+                
+                console.log($scope.asset);
+                console.log($scope.assetNameView);
+//
+//                window.app.pullDataById(document.restfulUrl, {
+//                    action: 'pullAssetBalance',
+//                    data: {
+//                        assetId: $scope.asset
+//                    }
+//
+//                }).then(function (success) {
+//                    $scope.$apply(function () {
+//                        if (success.data == null || success.data == 0 || success.data == 'undefined') {
+//                            $('#IssueSubmitBtn').attr('disabled', 'disabled');
+//                        } else {
+//                            $('#IssueSubmitBtn').attr('disabled', false);
+//                        }
+//                        $scope.rQ = 'REM BALANCE: ' + success.data;
+//                        $scope.bal = success.data;
+//                        $("#quantity").attr({"max": success.data, "min": 1});
+//                    });
+//                }, function (error) {
+//                    console.log("error", error);
+//                })
 
 
             }
-            
-            
+
+
             $scope.radioClik = function () {
                 console.log('sdfdsf');
                 console.log($scope.rdChk);
@@ -63,29 +72,34 @@ angular.module('hris', [])
                     $("#returnDate").prop('required', true);
                 }
             }
-            
-            $scope.rdClk=function(){
+
+            $scope.rdClk = function () {
                 $scope.radioClik();
             }
-            
 
-            
+            $scope.openmod = function () {
+                console.log('sdfdsf');
+            }
+
+
+
             $("#assetSetupTable").on("click", "#btnIssue", function () {
+//                $('#myModal').modal('show');
                 $('#returnedDate').val('');
-                var issueButton =$(this);
-                var selectedassetId=issueButton.attr('data-assetid');
-                var selectedassetName=issueButton.attr('data-asset');
-                
+                var issueButton = $(this);
+                var selectedassetId = issueButton.attr('data-assetid');
+                var selectedassetName = issueButton.attr('data-asset');
+
                 $('#requestDate').val('');
                 $('#issueDate').val('');
                 $('#quantity').val('');
                 $('#purposeTA').val('');
                 $('#remarks').val('');
                 $("#returnDate").val('');
-                
-                
-                $scope.$apply(function(){
-                $scope.assetIssue(selectedassetName, selectedassetId);
+
+
+                $scope.$apply(function () {
+                    $scope.assetIssue(selectedassetName, selectedassetId);
                 });
             });
 
@@ -111,12 +125,61 @@ angular.module('hris', [])
 
 
 
-            
 
 
 
 
-        }).directive('assetissue', function ($parse) {
+            $ctrl = this;
+            $ctrl.animationsEnabled = false;
+            $scope.assetIssueBtn = function (assetName,assetId) {
+                
+//                console.log(assetName);
+//                console.log(assetId);
+                var modalInstance = $uibModal.open({
+                    animation: $ctrl.animationsEnabled,
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'myModalContent.html',
+                    controller: function ($scope, $uibModalInstance) {
+                        
+                    $scope.assetIssue(assetName, assetId);
+//                            $scope.assetNameView = assetName;
+                        
+                        $scope.assetIssuecancel = function () {
+                            $uibModalInstance.dismiss('cancel');
+                        };
+                    },
+                     controllerAs: '$ctrl'
+                });
+                
+                modalInstance.result.then(function (result) {
+                    console.log(result);
+                });
+                
+
+                modalInstance.rendered.then(function () {
+                    $("select").select2();
+                    app.addDatePicker($('#issueDate'));
+                    app.addDatePicker($('#requestDate'));
+                    app.addDatePicker($('#returnDate'));
+                });
+
+
+            };
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        ).directive('assetissue', function ($parse) {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
