1a19814e8ca5ab17e11859bf63811e720b4a349d
prabin <prabin.maharjan@itnepal.com>
2017-06-30 13:27:06 +0545

    commit

--- module/Application/src/Repository/CheckoutRepository.php
+++ module/Application/src/Repository/CheckoutRepository.php
@@ -35,13 +35,24 @@ class CheckoutRepository implements RepositoryInterface {
 
     public function fetchEmployeeShfitDetails($employeeId) {
         $sql = "SELECT TO_CHAR(SYSDATE, 'HH24:MI:SS') AS CURRENT_TIME,
-            TO_CHAR(S.END_TIME, 'HH24:MI:SS') AS CHECKOUT_TIME, 
+            TO_CHAR(S.END_TIME-((S.EARLY_OUT*60)/86400), 'HH24:MI:SS') AS CHECKOUT_TIME, 
             ESA.*,S.* FROM HRIS_EMPLOYEES E
                 join HRIS_EMPLOYEE_SHIFT_ASSIGN ESA on (ESA.EMPLOYEE_ID=E.EMPLOYEE_ID)
                 JOIN HRIS_SHIFTS S ON (S.SHIFT_ID=ESA.SHIFT_ID)
                 WHERE E.EMPLOYEE_ID=$employeeId AND ESA.STATUS='E' AND ESA.MODIFIED_DT IS NULL
                 AND (TO_DATE(TRUNC(SYSDATE), 'DD-MON-YY') BETWEEN TO_DATE(S.START_DATE, 'DD-MON-YY') AND TO_DATE(S.END_DATE, 'DD-MON-YY'))";
         
+        $statement=$this->adapter->query($sql);
+        $result=$statement->execute();
+        return $result->current();
+    }
+    
+    public function fetchEmployeeDefaultShift(){
+        $sql = "SELECT TO_CHAR(SYSDATE, 'HH24:MI:SS') AS CURRENT_TIME,
+      TO_CHAR(S.END_TIME-((S.EARLY_OUT*60)/86400), 'HH24:MI:SS')   AS CHECKOUT_TIME,S.*
+            FROM HRIS_SHIFTS S
+            WHERE S.DEFAULT_SHIFT='Y'
+            AND (TO_DATE(TRUNC(SYSDATE), 'DD-MON-YY') BETWEEN TO_DATE(S.START_DATE, 'DD-MON-YY') AND TO_DATE(S.END_DATE, 'DD-MON-YY'))";
         $statement=$this->adapter->query($sql);
         $result=$statement->execute();
         return $result->current();
