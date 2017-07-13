0eb731cd86179921a1aa97259880a54c3173351c
punam <alcute56@gmail.com>
2017-07-13 12:42:08 +0545

    changes

--- data/database/databases/JGI.sql
+++ data/database/databases/JGI.sql
@@ -53,3 +53,44 @@ ALTER TABLE HRIS_EMPLOYEES
 ADD IS_HR CHAR (1 BYTE) CHECK (IS_HR IN ('Y','N'));
 
 
+INSERT
+INTO HRIS_MENUS
+  (
+    MENU_CODE,
+    MENU_ID,
+    MENU_NAME,
+    PARENT_MENU,
+    MENU_DESCRIPTION,
+    ROUTE,
+    STATUS,
+    CREATED_DT,
+    MODIFIED_DT,
+    ICON_CLASS,
+    ACTION,
+    MENU_INDEX,
+    CREATED_BY,
+    MODIFIED_BY,
+    IS_VISIBLE
+  )
+  VALUES
+  (
+    'EMP',
+    325,
+    'Edit My Profile',
+    6,
+    NULL,
+    'employee',
+    'E',
+    to_date('13-JUL-17','DD-MON-RR'),
+    NULL,
+    'fa fa-wrench',
+    'edit',
+    44,
+    NULL,
+    NULL,
+    'N'
+  );
+INSERT INTO HRIS_ROLE_PERMISSIONS
+  (MENU_ID,ROLE_ID,STATUS
+  )
+SELECT (325),ROLE_ID, ('E') FROM HRIS_ROLES;
