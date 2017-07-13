4663df7462ea2621143e61d1a4c8b441c3fdff31
prabin <prabin.maharjan@itnepal.com>
2017-07-13 12:25:38 +0545

    commit

--- data/database/databases/ITNEPAL_HRIS_APR2.sql
+++ data/database/databases/ITNEPAL_HRIS_APR2.sql
@@ -26,3 +26,43 @@ ADD IS_CEO CHAR (1 BYTE) CHECK (IS_CEO IN ('Y','N'));
 
 ALTER TABLE HRIS_EMPLOYEES
 ADD IS_HR CHAR (1 BYTE) CHECK (IS_HR IN ('Y','N'));
+
+
+
+INSERT
+INTO HRIS_MENUS
+  (
+    MENU_CODE,
+    MENU_ID,
+    MENU_NAME,
+    PARENT_MENU,
+    MENU_DESCRIPTION,
+    ROUTE,
+    STATUS,
+    CREATED_DT,
+    MODIFIED_DT,
+    ICON_CLASS,
+    ACTION,
+    MENU_INDEX,
+    CREATED_BY,
+    MODIFIED_BY,
+    IS_VISIBLE
+  )
+  VALUES
+  (
+    NULL,
+    328,
+    'Attendnace Report',
+    5,
+    NULL,
+    'managerReport',
+    'E',
+    TRUNC(SYSDATE),
+    NULL,
+    'fa fa-pencil',
+    'index',
+    19,
+    NULL,
+    NULL,
+    'Y'
+  );
