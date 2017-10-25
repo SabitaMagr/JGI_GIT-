801de4830bdaa05234bf45b462ae6a91f51f33b5
Himal Rai <himal.rai@itnepal.com>
2017-10-25 18:03:14 +0545

    trainig hr and manager change

--- public/js/training/training-status/index.js
+++ public/js/training/training-status/index.js
@@ -2,253 +2,90 @@
     'use strict';
     $(document).ready(function () {
         $("select").select2();
-        app.startEndDatePickerWithNepali('nepaliFromDate', 'fromDate1', 'nepaliToDate', 'toDate1', null, true);
+        app.startEndDatePickerWithNepali('nepaliFromDate', 'fromDate', 'nepaliToDate', 'toDate', null, true);
+        var $tableContainer = $("#trainingRequestStatusTable");
+        var $search = $('#search');
+
+        var columns = [
+            {field: "FULL_NAME", title: "Employee"},
+            {field: "TITLE", title: "Training"},
+            {title: "Requested Date",
+                columns: [{
+                        field: "REQUESTED_DATE_AD",
+                        title: "AD",
+                    },
+                    {
+                        field: "REQUESTED_DATE_BS",
+                        title: "BS",
+                    }]},
+            {title: "Start Date",
+                columns: [{
+                        field: "START_DATE_AD",
+                        title: "AD",
+                    },
+                    {
+                        field: "START_DATE_BS",
+                        title: "BS",
+                    }]},
+            {title: "End Date",
+                columns: [{
+                        field: "END_DATE_AD",
+                        title: "AD",
+                    },
+                    {
+                        field: "END_DATE_BS",
+                        title: "BS",
+                    }]},
+            {field: "DURATION", title: "Duration"},
+            {field: "TRAINING_TYPE", title: "Training Type"},
+            {field: "STATUS", title: "Status"},
+            {field: ["REQUEST_ID"], title: "Action", template: `
+            <span> 
+                <a class="btn  btn-icon-only btn-success" href="${document.viewLink}/#: REQUEST_ID #" style="height:17px;" title="view">
+                    <i class="fa fa-search-plus"></i>
+                </a>
+            </span>`}];
+        var map = {
+            'FULL_NAME': 'Name',
+            'TRAINING_NAME': 'Training',
+            'REQUESTED_DATE_AD': 'Request Date(AD)',
+            'REQUESTED_DATE_BS': 'Request Date(BS)',
+            'START_DATE_AD': 'Start Date(AD)',
+            'START_DATE_BS': 'Start Date(BS)',
+            'END_DATE_AD': 'End Date(AD)',
+            'END_DATE_BS': 'End Date(BS)',
+            'DURATION': 'Duration',
+            'TRAINING_TYPE': 'Type',
+            'STATUS': 'Status',
+            'DESCRIPTION': 'Description',
+            'REMARKS': 'Remarks',
+            'RECOMMENDED_REMARKS': 'Recommended Remarks',
+            'RECOMMENDED_DATE': 'Recommended Date',
+            'APPROVED_REMARKS': 'Approved Remarks',
+            'APPROVED_DATE': 'Approved Date'
+        };
+        app.initializeKendoGrid($tableContainer, columns, "Training Request List.xlsx");
+        app.searchTable($tableContainer, ['FULL_NAME']);
+
+        $search.on('click', function () {
+            var q = document.searchManager.getSearchValues();
+            q['requestStatusId'] = $('#requestStatusId').val();
+            q['fromDate'] = $('#fromDate').val();
+            q['toDate'] = $('#toDate').val();
+            q['recomApproveId'] = $('#recomApproveId').val();
+            App.blockUI({target: "#hris-page-content"});
+            app.pullDataById(document.pullTrainingRequestStatusListLink, q).then(function (success) {
+                App.unblockUI("#hris-page-content");
+                app.renderKendoGrid($tableContainer, success.data);
+            }, function (failure) {
+                App.unblockUI("#hris-page-content");
+            });
+        });
+        $('#excelExport').on('click', function () {
+            app.excelExport($tableContainer, map, "Training Request List.xlsx");
+        });
+        $('#pdfExport').on('click', function () {
+            app.exportToPDF($tableContainer, map, "Training Request List.pdf");
+        });
     });
 })(window.jQuery, window.app);
-
-angular.module('hris', [])
-        .controller("trainingStatusListController", function ($scope, $http) {
-            var $tableContainer = $("#trainingRequestStatusTable");
-            var displayKendoFirstTime = true;
-            $scope.view = function () {
-                var employeeId = angular.element(document.getElementById('employeeId')).val();
-                var companyId = angular.element(document.getElementById('companyId')).val();
-                var branchId = angular.element(document.getElementById('branchId')).val();
-                var departmentId = angular.element(document.getElementById('departmentId')).val();
-                var designationId = angular.element(document.getElementById('designationId')).val();
-                var positionId = angular.element(document.getElementById('positionId')).val();
-                var serviceEventTypeId = angular.element(document.getElementById('serviceEventTypeId')).val();
-                var serviceTypeId = angular.element(document.getElementById('serviceTypeId')).val();
-                var requestStatusId = angular.element(document.getElementById('requestStatusId')).val();
-                var fromDate = angular.element(document.getElementById('fromDate1')).val();
-                var toDate = angular.element(document.getElementById('toDate1')).val();
-                var employeeTypeId = angular.element(document.getElementById('employeeTypeId')).val();
-                App.blockUI({target: "#hris-page-content"});
-                window.app.pullDataById(document.pullTrainingRequestStatusListLink, {
-                    'employeeId': employeeId,
-                    'companyId': companyId,
-                    'branchId': branchId,
-                    'departmentId': departmentId,
-                    'designationId': designationId,
-                    'positionId': positionId,
-                    'serviceTypeId': serviceTypeId,
-                    'serviceEventTypeId': serviceEventTypeId,
-                    'requestStatusId': requestStatusId,
-                    'fromDate': fromDate,
-                    'toDate': toDate,
-                    'employeeTypeId': employeeTypeId
-                }).then(function (success) {
-                    App.unblockUI("#hris-page-content");
-                    console.log(success.data);
-                    if (displayKendoFirstTime) {
-                        $scope.initializekendoGrid();
-                        displayKendoFirstTime = false;
-                    }
-                    var dataSource = new kendo.data.DataSource({data: success.data, pageSize: 20});
-                    var grid = $('#trainingRequestStatusTable').data("kendoGrid");
-                    dataSource.read();
-                    grid.setDataSource(dataSource);
-                }, function (failure) {
-                    App.unblockUI("#hris-page-content");
-                    console.log(failure);
-                });
-            }
-            $scope.initializekendoGrid = function () {
-                $("#trainingRequestStatusTable").kendoGrid({
-                    excel: {
-                        fileName: "TrainingRequestList.xlsx",
-                        filterable: true,
-                        allPages: true
-                    },
-                    height: 450,
-                    scrollable: true,
-                    sortable: true,
-                    filterable: true,
-                    pageable: {
-                        input: true,
-                        numeric: false
-                    },
-                    dataBound: gridDataBound,
-//                    rowTemplate: kendo.template($("#rowTemplate").html()),
-                    columns: [
-                        {field: "FULL_NAME", title: "Employee"},
-                        {field: "TRAINING_NAME", title: "Training", template: "<span> #: (TITLE == null) ? '-' : TITLE #</span>"},
-//                        {field: "REQUESTED_DATE", title: "Requested Date", width: 110},
-                        {title: "Requested Date",
-                            columns: [{
-                                    field: "REQUESTED_DATE",
-                                    title: "AD",
-                                    template: "<span>#: (REQUESTED_DATE == null) ? '-' : REQUESTED_DATE #</span>"},
-                                {field: "REQUESTED_DATE_N",
-                                    title: "BS",
-                                    template: "<span>#: (REQUESTED_DATE_N == null) ? '-' : REQUESTED_DATE_N #</span>"}]},
-//                        {field: "START_DATE", title: "Start Date", width: 110},
-                        {title: "Start Date",
-                            columns: [{
-                                    field: "START_DATE",
-                                    title: "AD",
-                                    template: "<span>#: (START_DATE == null) ? '-' : START_DATE #</span>"},
-                                {field: "START_DATE_N",
-                                    title: "BS",
-                                    template: "<span>#: (START_DATE_N == null) ? '-' : START_DATE_N #</span>"}]},
-                        {title: "End Date",
-                            columns: [{
-                                    field: "END_DATE",
-                                    title: "AD",
-                                    template: "<span>#: (END_DATE == null) ? '-' : END_DATE #</span>"},
-                                {field: "END_DATE_N",
-                                    title: "BS",
-                                    template: "<span>#: (END_DATE_N == null) ? '-' : END_DATE_N #</span>"}]},
-//                        {field: "END_DATE", title: "End Date", width: 110},
-                        {field: "DURATION", title: "Duration"},
-                        {field: "TRAINING_TYPE", title: "Type"},
-                        {field: "RECOMMENDER_NAME", title: "Recommender"},
-                        {field: "APPROVER_NAME", title: "Approver"},
-                        {field: "STATUS", title: "Status"},
-                        {field: ["REQUEST_ID"], title: "Action", template: `<span><a class="btn-edit"
-    href="` + document.viewLink + `/#: REQUEST_ID #" style="height:17px;" title="view">
-    <i class="fa fa-search-plus"></i>
-    </a></span>`}
-                    ]
-                });
-
-                app.searchTable('trainingRequestStatusTable', ['FULL_NAME', 'TRAINING_NAME', 'REQUESTED_DATE', 'START_DATE', 'END_DATE', 'REQUESTED_DATE_N', 'START_DATE_N', 'END_DATE_N', 'DURATION', 'TRAINING_TYPE', 'RECOMMENDER_NAME', 'APPROVER_NAME', 'STATUS']);
-
-                app.pdfExport(
-                        'trainingRequestStatusTable',
-                        {
-                            'FULL_NAME': 'Name',
-                            'TRAINING_NAME': 'Training',
-                            'REQUESTED_DATE': 'Request Date(AD)',
-                            'REQUESTED_DATE_N': 'Request Date(BS)',
-                            'START_DATE': 'Start Date(AD)',
-                            'START_DATE_N': 'Start Date(BS)',
-                            'END_DATE': 'End Date(AD)',
-                            'END_DATE_N': 'End Date(BS)',
-                            'DURATION': 'Duration',
-                            'TRAINING_TYPE': 'Training Type',
-                            'RECOMMENDER_NAME': 'Recommender',
-                            'APPROVER_NAME': 'Approver',
-                            'STATUS': 'Status',
-                            'DESCRIPTION': 'Description',
-                            'REMARKS': 'Remarks',
-                            'RECOMMENDED_REMARKS': 'Recommender Remarks',
-                            'RECOMMENDED_DATE': 'Recommended Date',
-                            'APPROVED_REMARKS': 'Approver Remarks',
-                            'APPROVED_DATE': 'Approved Date',
-                        });
-
-
-                function gridDataBound(e) {
-                    var grid = e.sender;
-                    if (grid.dataSource.total() == 0) {
-                        var colCount = grid.columns.length;
-                        $(e.sender.wrapper)
-                                .find('tbody')
-                                .append('<tr class="kendo-data-row"><td colspan="' + colCount + '" class="no-data">There is no data to show in the grid.</td></tr>');
-                    }
-                }
-                ;
-
-                $("#export").click(function (e) {
-                    var rows = [{
-                            cells: [
-                                {value: "Employee Name"},
-                                {value: "Training Name"},
-                                {value: "Requested Date(AD)"},
-                                {value: "Requested Date(BS)"},
-                                {value: "Start Date(AD)"},
-                                {value: "Start Date(BS)"},
-                                {value: "End Date(AD)"},
-                                {value: "End Date(BS)"},
-                                {value: "Duration"},
-                                {value: "Training Type"},
-                                {value: "Recommender"},
-                                {value: "Approver"},
-                                {value: "Status"},
-                                {value: "Description"},
-                                {value: "Remarks"},
-                                {value: "Remarks By Recommender"},
-                                {value: "Recommended Date"},
-                                {value: "Remarks By Approver"},
-                                {value: "Approved Date"}
-                            ]
-                        }];
-                    var dataSource = $("#trainingRequestStatusTable").data("kendoGrid").dataSource;
-                    var filteredDataSource = new kendo.data.DataSource({
-                        data: dataSource.data(),
-                        filter: dataSource.filter()
-                    });
-
-                    filteredDataSource.read();
-                    var data = filteredDataSource.view();
-
-                    for (var i = 0; i < data.length; i++) {
-                        var dataItem = data[i];
-                        var mn1 = dataItem.MN1 != null ? " " + dataItem.MN1 + " " : " ";
-                        var mn2 = dataItem.MN2 != null ? " " + dataItem.MN2 + " " : " ";
-                        rows.push({
-                            cells: [
-                                {value: dataItem.FULL_NAME},
-                                {value: dataItem.TRAINING_NAME},
-                                {value: dataItem.REQUESTED_DATE},
-                                {value: dataItem.REQUESTED_DATE_N},
-                                {value: dataItem.START_DATE},
-                                {value: dataItem.START_DATE_N},
-                                {value: dataItem.END_DATE},
-                                {value: dataItem.END_DATE_N},
-                                {value: dataItem.DURATION},
-                                {value: dataItem.TRAINING_TYPE},
-                                {value: dataItem.RECOMMENDER_NAME},
-                                {value: dataItem.APPROVER_NAME},
-                                {value: dataItem.STATUS},
-                                {value: dataItem.DESCRIPTION},
-                                {value: dataItem.REMARKS},
-                                {value: dataItem.RECOMMENDED_REMARKS},
-                                {value: dataItem.RECOMMENDED_DATE},
-                                {value: dataItem.APPROVED_REMARKS},
-                                {value: dataItem.APPROVED_DATE}
-                            ]
-                        });
-                    }
-                    excelExport(rows);
-                    e.preventDefault();
-                });
-
-                function excelExport(rows) {
-                    var workbook = new kendo.ooxml.Workbook({
-                        sheets: [
-                            {
-                                columns: [
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true},
-                                    {autoWidth: true}
-
-                                ],
-                                title: "Training Request",
-                                rows: rows
-                            }
-                        ]
-                    });
-                    kendo.saveAs({dataURI: workbook.toDataURL(), fileName: "TrainingRequestList.xlsx"});
-                }
-
-                window.app.UIConfirmations();
-            };
-        });
