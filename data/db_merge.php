b24d142a11e838307fd780e6d070549d921c00aa
Himal Rai <himal.rai@itnepal.com>
2018-01-15 10:55:45 +0545

    changes

---
+++ data/db_merge.php
@@ -0,0 +1,20 @@
+<?php
+
+$dirList = ["procedure", "triggers", "function"];
+$fileDataAll = "";
+
+
+foreach ($dirList as $dir) {
+    $scanned_directory = array_diff(scandir($dir), array('..', '.'));
+    foreach ($scanned_directory as $filename) {
+        $file = $dir . "/" . $filename;
+        if (is_file($file)) {
+            $fileDataAll .= file_get_contents($file);
+        }
+    }
+}
+
+$my_file = 'database/fns_trgs_procs.sql';
+$handle = fopen($my_file, 'w') or die('Cannot open file:  ' . $my_file);
+fwrite($handle, $fileDataAll);
+
