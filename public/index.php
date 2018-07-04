cd8415a2863c518d6b0772310a3964dee9bea331
prabin <prabin.maharjan@itnepal.com>
2018-07-04 14:52:11 +0545

    register attendnace updated not working fixed

--- public/index.php
+++ public/index.php
@@ -13,6 +13,10 @@ if ('development' == APPLICATION_ENV || 'staging' == APPLICATION_ENV) {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 }
+else{
+    error_reporting(0);
+    ini_set("display_errors", 0);
+}
 
 /**
  * This makes our life easier when dealing with paths. Everything is relative
