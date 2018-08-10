a8f4033476a057bf6485fdb99e812943a7b2fe2e
prabin <prabin.maharjan@itnepal.com>
2018-08-10 12:12:32 +0545

    changes

--- data/function/LEAVE_STATUS_DESC.sql
+++ data/function/LEAVE_STATUS_DESC.sql
@@ -1,22 +1,15 @@
-create or replace FUNCTION LEAVE_STATUS_DESC(
-    P_STATUS HRIS_EMPLOYEE_LEAVE_REQUEST.STATUS%TYPE)
-  RETURN VARCHAR2
-IS
-  V_STATUS_DESC VARCHAR2(50 BYTE);
+create or replace FUNCTION leave_status_desc ( p_status hris_employee_leave_request.status%TYPE ) RETURN VARCHAR2 IS
+    v_status_desc   VARCHAR2(50 BYTE);
 BEGIN
-  V_STATUS_DESC:=
-  (
-    CASE P_STATUS
-    WHEN 'RQ' THEN
-      'Pending'
-    WHEN 'RC'THEN
-      'Recommended'
-    WHEN 'R' THEN
-      'Rejected'
-    WHEN 'AP' THEN
-      'Approved'
-    WHEN 'C' THEN
-      'Cancelled'
-    END);
-  RETURN V_STATUS_DESC;
+    v_status_desc := ( CASE p_status
+        WHEN 'RQ' THEN 'Pending'
+        WHEN 'RC' THEN 'Recommended'
+        WHEN 'R' THEN 'Rejected'
+        WHEN 'AP' THEN 'Approved'
+        WHEN 'C' THEN 'Cancelled'
+        WHEN 'CP' THEN 'C Pending'
+        WHEN 'CR' THEN 'C Recommended'
+    END );
+
+    RETURN v_status_desc;
 END;
