32455c78e475106551f28f02c683f35db585dde3
prabin <prabin.maharjan@itnepal.com>
2018-08-23 12:23:01 +0545

    attendance by hr half day also accumulated

--- data/procedure/HRIS_MANUAL_ATTENDANCE.sql
+++ data/procedure/HRIS_MANUAL_ATTENDANCE.sql
@@ -3,13 +3,20 @@ CREATE OR REPLACE PROCEDURE HRIS_MANUAL_ATTENDANCE(
     P_ATTENDANCE_DT HRIS_ATTENDANCE_DETAIL.ATTENDANCE_DT%TYPE,
     P_STATUS CHAR )
 AS
+  V_WEEK_DAY NUMBER(1);
+    V_DYNAMIC_SQL VARCHAR2(1000 BYTE);
+    V_TO_TIME TIMESTAMP;
 BEGIN
+
+select to_char(p_attendance_dt, 'd') INTO V_WEEK_DAY from dual;
+
   FOR attendance IN
   (SELECT A.EMPLOYEE_ID,
     A.ATTENDANCE_DT,
     S.START_TIME,
     S.END_TIME,
-    A.OVERALL_STATUS
+    A.OVERALL_STATUS,
+    s.shift_id
   FROM HRIS_ATTENDANCE_DETAIL A
   JOIN HRIS_SHIFTS S
   ON (A.SHIFT_ID   =S.SHIFT_ID)
@@ -18,6 +25,25 @@ BEGIN
   AND A.OVERALL_STATUS IN ('AB','PR','BA','LA')
   )
   LOOP
+  
+   V_TO_TIME:=attendance.end_time;
+    
+    
+    -- begin for overriding halfday case
+    
+    V_DYNAMIC_SQL:='SELECT CASE WHEN WEEKDAY'||V_WEEK_DAY||'=''H'' AND HALF_DAY_OUT_TIME IS NOT NULL  THEN
+            HALF_DAY_OUT_TIME
+            ELSE
+            end_time
+            END
+            FROM  HRIS_SHIFTS WHERE SHIFT_ID='||attendance.shift_id;
+            
+            execute immediate V_DYNAMIC_SQL into V_TO_TIME;
+
+    -- end for overriding halfday case
+  
+  
+  
     IF P_STATUS ='P' THEN
       INSERT
       INTO HRIS_ATTENDANCE
@@ -50,7 +76,7 @@ BEGIN
           attendance.ATTENDANCE_DT,
           TO_DATE(TO_CHAR(attendance.ATTENDANCE_DT,'DD-MON-YYYY')
           ||' '
-          ||TO_CHAR(attendance.END_TIME,'HH24:MI'),'DD-MON-YYYY HH24:MI' ),
+          ||TO_CHAR(V_TO_TIME,'HH24:MI'),'DD-MON-YYYY HH24:MI' ),
           'SYSTEM'
         );
     END IF;
