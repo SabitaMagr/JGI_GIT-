6a548218586591bf49d9f432394ad0c18f99383d
prabin <prabin.maharjan@itnepal.com>
2019-03-12 14:28:41 +0545

    selfservice profile lock

--- public/js/SelfService/Profile/lock.js
+++ public/js/SelfService/Profile/lock.js
@@ -2,7 +2,16 @@
     'use strict';
     $(document).ready(function () {
         if (document.employeeId == document.selfEmployeeId) {
-            app.lockField(true, ['birthdate', 'firstName', 'middleName', 'lastName', 'nameNepali', 'nepaliBirthDate', 'companyId', 'idCardNo', 'idThumbId', 'idLbrf', 'tab4', 'tab5', 'tab7', 'tab8']);
+            app.lockField(true, [
+			'employeeCode',
+			'genderId',
+			'countryId',
+			'religionId',
+			'emailOfficial',
+			'MobileNo',
+			'bloodGroupId',
+                        'addrPermHouseNo',
+			'birthdate', 'firstName', 'middleName', 'lastName', 'nameNepali', 'nepaliBirthDate', 'companyId', 'idCardNo', 'idThumbId', 'idLbrf', 'tab1', 'tab2', 'tab3', 'tab4', 'tab5', 'tab7','tab1', 'tab8']);
         }
     });
 })(window.jQuery, window.app);
