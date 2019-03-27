e8339bcf6bf6c4cb4d43eb6ec6912e880cc9ce0c
prabin <prabin.maharjan@itnepal.com>
2019-03-27 15:51:14 +0545

    fn trigeer update

--- data/database/fns_trgs_procs.sql
+++ data/database/fns_trgs_procs.sql
@@ -1300,6 +1300,46 @@ BEGIN
     END LOOP;
   END;
 END;/
+            create or replace PROCEDURE HRIS_FIX_SUB_LEAVE
+AS
+begin
+for LEAVE_LIST in (select * from (SELECT LA.LEAVE_ID,la.employee_id,
+                  LMS.LEAVE_CODE,
+                  LMS.LEAVE_ENAME,
+                  LA.PREVIOUS_YEAR_BAL,
+                  LA.TOTAL_DAYS,
+                  LA.BALANCE,
+                  (SELECT SUM(ELR.NO_OF_DAYS/(
+                    CASE
+                      WHEN ELR.HALF_DAY IN ('F','S')
+                      THEN 2
+                      ELSE 1
+                    END))
+                  FROM HRIS_EMPLOYEE_LEAVE_REQUEST ELR
+                   LEFT JOIN (SELECT * FROM HRIS_LEAVE_YEARS  WHERE TRUNC(SYSDATE) BETWEEN START_DATE AND END_DATE ) LY ON (1=1)
+                  WHERE ELR.LEAVE_ID =LA.LEAVE_ID
+                  AND ELR.EMPLOYEE_ID=LA.EMPLOYEE_ID
+                  AND ELR.STATUS     ='AP'
+                   AND ELR.START_DATE BETWEEN LY.START_DATE AND LY.END_DATE
+                  ) AS LEAVE_TAKEN,
+                  (SELECT SUM(ELA.NO_OF_DAYS)
+                  FROM HRIS_EMPLOYEE_LEAVE_ADDITION ELA
+                  WHERE ELA.EMPLOYEE_ID=LA.EMPLOYEE_ID
+                  AND ELA.LEAVE_ID     =LA.LEAVE_ID
+                  ) AS LEAVE_ADDED
+                FROM HRIS_EMPLOYEE_LEAVE_ASSIGN LA
+                LEFT JOIN HRIS_LEAVE_MASTER_SETUP LMS
+                ON (LA.LEAVE_ID     =LMS.LEAVE_ID)
+                WHERE LMS.IS_SUBSTITUTE='Y' AND LMS.STATUS ='E' 
+                AND LMS.IS_MONTHLY = 'N' ORDER BY LMS.LEAVE_ENAME ASC)
+                where 
+                leave_taken>total_days or leave_added!=total_days)
+loop
+HRIS_RECALCULATE_LEAVE(LEAVE_LIST.EMPLOYEE_ID,LEAVE_LIST.LEAVE_ID);
+end loop;
+end;
+ 
+ /
             CREATE OR REPLACE PROCEDURE HRIS_GEN_SAL_SH_REPORT(
     P_SHEET_NO HRIS_SALARY_SHEET.SHEET_NO%TYPE )
 AS
@@ -1645,6 +1685,7 @@ BEGIN
         EXECUTED_AT =SYSDATE
       WHERE JOB_ID  = jobs.JOB_ID;
     END;
+COMMIT;
   END LOOP;
 END;/
             CREATE OR REPLACE PROCEDURE HRIS_LATE_LEAVE_DEDUCTION(
@@ -5445,9 +5486,9 @@ BEGIN
     -- check  total working hour  
     --if greater than 2 then add 0.5 leave if  greater tan 4 then 1 day leave 
     IF((V_TOTAL_HOUR /60)     >= 2 AND(V_TOTAL_HOUR /60) < 4) THEN
-      V_INCREMENT_DAY         :=.5;
+      V_INCREMENT_DAY         :=V_INCREMENT_DAY+.5;
     ELSIF ((V_TOTAL_HOUR /60) >=4) THEN
-      V_INCREMENT_DAY         :=1;
+      V_INCREMENT_DAY         :=V_INCREMENT_DAY+1;
     END IF;
   END LOOP;
   --
@@ -5753,9 +5794,9 @@ BEGIN
     END IF;
     --
     IF((V_TOTAL_HOUR /60) >= 2 AND (V_TOTAL_HOUR /60) < 4) THEN
-      V_INCREMENT_DAY         :=.5;
+      V_INCREMENT_DAY         :=(V_INCREMENT_DAY+.5);
     ELSIF ((V_TOTAL_HOUR /60) >=4) THEN
-      V_INCREMENT_DAY         :=1;
+      V_INCREMENT_DAY         :=(V_INCREMENT_DAY+1);
     END IF;
   END LOOP;
 --
@@ -6262,7 +6303,8 @@ BEGIN
                     trunc(SYSDATE),
                     NULL,
                     NULL,
-                    p_training_id
+                    p_training_id,
+                    NULL
                 );
 
             END IF;
