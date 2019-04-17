5a7d59e88875b70d2053f98dd09ef9c9908a89da
prabin <prabin.maharjan@itnepal.com>
2019-04-17 14:53:54 +0545

    manager service delted employee leave shown

--- module/LeaveManagement/src/Repository/LeaveStatusRepository.php
+++ module/LeaveManagement/src/Repository/LeaveStatusRepository.php
@@ -181,30 +181,6 @@ class LeaveStatusRepository extends HrisRepository {
                 )
                 WHERE L.STATUS   ='E'
                 AND E.STATUS     ='E'
-                AND (E1.STATUS   =
-                  CASE
-                    WHEN E1.STATUS IS NOT NULL
-                    THEN ('E')
-                  END
-                OR E1.STATUS  IS NULL)
-                AND (E2.STATUS =
-                  CASE
-                    WHEN E2.STATUS IS NOT NULL
-                    THEN ('E')
-                  END
-                OR E2.STATUS    IS NULL)
-                AND (RECM.STATUS =
-                  CASE
-                    WHEN RECM.STATUS IS NOT NULL
-                    THEN ('E')
-                  END
-                OR RECM.STATUS  IS NULL)
-                AND (APRV.STATUS =
-                  CASE
-                    WHEN APRV.STATUS IS NOT NULL
-                    THEN ('E')
-                  END
-                OR APRV.STATUS       IS NULL)
                 AND (LS.APPROVED_FLAG =
                   CASE
                     WHEN LS.EMPLOYEE_ID IS NOT NULL
