fc7868a294cdd12638fae9b6e0268d308e4ca5e1
prabin <prabin.maharjan@itnepal.com>
2019-09-06 17:31:47 +0545

    role wise super power approver in  leave attd wod and woh

--- module/LeaveManagement/src/Controller/LeaveStatus.php
+++ module/LeaveManagement/src/Controller/LeaveStatus.php
@@ -149,6 +149,7 @@ class LeaveStatus extends HrisController {
                     'subApprovedFlag' => $detail['SUB_APPROVED_FLAG'],
                     'employeeList' => EntityHelper::getTableKVListWithSortOption($this->adapter, HrEmployees::TABLE_NAME, HrEmployees::EMPLOYEE_ID, [HrEmployees::FIRST_NAME, HrEmployees::MIDDLE_NAME, HrEmployees::LAST_NAME], [HrEmployees::STATUS => "E", HrEmployees::RETIRED_FLAG => "N"], HrEmployees::FIRST_NAME, "ASC", " ", FALSE, TRUE),
                     'gp' => $detail['GRACE_PERIOD'],
+                    'acl' => $this->acl,
                     'files' => $fileDetails
         ]);
     }
