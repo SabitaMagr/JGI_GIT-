6c5be297ccd6a76ead0db9904d5b8d059dfbeea6
prabin <prabin.maharjan@itnepal.com>
2019-10-31 10:47:56 +0545

    hris overtime validation

--- data/function/HRIS_VALIDATE_OVERTIME_ATTD.sql
+++ data/function/HRIS_VALIDATE_OVERTIME_ATTD.sql
@@ -4,15 +4,6 @@ create or replace function hris_validate_overtime_attd(
 ) 
 return char
 as
-    V_total_worked_minutes integer;
 begin
-select total_hour into V_total_worked_minutes from hris_attendance_detail where 
-employee_id = p_employee_id and 
-attendance_dt = p_attendance_dt;
-
-if V_total_worked_minutes > 600 then
 return 'T';
-else
-return 'F';
-end if;
 end;
