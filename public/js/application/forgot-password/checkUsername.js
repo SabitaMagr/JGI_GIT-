a96eaa9246311486e98e661c365019f5ce7fbd65
pramod <pramod@itnepal.com>
2020-07-02 13:50:39 +0545

    reattedance proc updated

--- public/js/application/forgot-password/checkUsername.js
+++ public/js/application/forgot-password/checkUsername.js
@@ -1,16 +1,16 @@
 (function ($, app) {
     'use strict';
-    $("#username").on("blur", function () {
-        var username = $("#username").val();
-        for (var i = 0; i < document.userList.length; i++) {
-            if (document.userList[i].USER_NAME === username) {
-                $(".errorMsg").html("");
-                return true;
-            }
-        }
-        ;
-        $(".errorMsg").html("There is no account registered for this username!!!.")
-    });
+//    $("#username").on("blur", function () {
+//        var username = $("#username").val();
+//        for (var i = 0; i < document.userList.length; i++) {
+//            if (document.userList[i].USER_NAME === username) {
+//                $(".errorMsg").html("");
+//                return true;
+//            }
+//        }
+//        ;
+//        $(".errorMsg").html("There is no account registered for this username!!!.")
+//    });
     $('#usernameForm').on("submit",function (e) {
         var err = [];
         $(".errorMsg").each(function () {
